2b7964316a0f4bc33bd9aee4df99ba5db7179b13
Jasonzhi123 <1244653559@qq.com>
2017-10-09 13:41:17 +0800

    3

--- application/api/controller/Home.php
+++ application/api/controller/Home.php
@@ -10,10 +10,25 @@
 		function index()
 		{
 			$head_list=db("headline")
-				->field("id,title,content,create_time,expert_id,audio_id")
+				->alias('h')
+				->join("expert e","e.id=h.expert_id")
 				->select();
-
 				return json($head_list);
 		}
-	}
+		function smalltalk()
+		{
+			$special_list=db("special")
+				->alias('s')
+				->join("column c","c.id=s.column_id")
+				->join("expert e","e.id=c.expert_id")
+				->join("categories ca","e.classification_id=ca.id")
+				->select();
+				return json($special_list);
+		}
+		function smallList(){
+			$smallList=db("categories")
+				->select();
+				return json($smallList);
+		}
+	}   
  ?>
