3d0c1b6ee4b646cbfabce7a4d45018c22fb994be
Dave <948009662@qq.com>
2017-10-10 21:26:27 +0800

    fdfdf

--- application/api/controller/Islogin.php
+++ application/api/controller/Islogin.php
@@ -13,10 +13,6 @@
 		{
 			parent::__construct();
 			$this->login = Session::get('user');
-			header('Access-Control-Allow-Origin: http://localhost:8080');
-			header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-			header('Access-Control-Allow-Credentials: true');
-			header('Access-Control-Allow-Methods: GET, POST, PUT');
 			if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
 			    exit;
 			}
